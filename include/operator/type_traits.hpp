f8ec125873d1972f89331fd153c87b34fbeca183
Solomon <solomonknudson@gmail.com>
2026-05-20 23:51:16 -0400

    add new type trait wrappers

--- include/operator/type_traits.hpp
+++ include/operator/type_traits.hpp
@@ -6,6 +6,12 @@ namespace Operator::type_traits
 {
   template <typename T> using remove_reference = std::remove_reference_t<T>;
 
+  template <typename T>
+  using remove_cv_reference = remove_reference<std::remove_cv_t<T>>;
+
+  template <typename T>
+  using remove_pointer = std::remove_pointer_t<remove_cv_reference<T>>;
+
   template <typename, typename = void> struct can_deref : std::false_type
   {
   };
